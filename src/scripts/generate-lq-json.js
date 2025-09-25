ece82dd840a7bc9e945ade480d614b50532dca1b
Carolyn Sun <117613176+carolyn-sun@users.noreply.github.com>
2025-09-26 03:39:54 +0800

    add Flow component and JSON generation for low-quality images

--- src/scripts/generate-lq-json.js
+++ src/scripts/generate-lq-json.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const buildDir = path.resolve(__dirname, '../../build');
+const staticDir = path.resolve(__dirname, '../../static');
 const result = [];
 
 function walk(dir) {
@@ -12,13 +12,13 @@ function walk(dir) {
         if (stat.isDirectory()) {
             walk(fullPath);
         } else if (/lq.*\.(jpe?g|png|webp)$/i.test(file)) {
-            result.push(fullPath.replace(buildDir, '').replace(/\\/g, '/'));
+            result.push(fullPath.replace(staticDir, '').replace(/\\/g, '/'));
         }
     }
 }
 
-walk(buildDir);
+walk(staticDir);
 
-const output = path.join(buildDir, '../static/lqImages.json');
+const output = path.join(staticDir, 'lqImages.json');
 fs.writeFileSync(output, JSON.stringify(result, null, 2));
 console.log('lqImages.json generated.');
