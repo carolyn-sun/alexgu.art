024071da57177d7184d088c4f716d6f6398450bf
Carolyn Sun <117613176+carolyn-sun@users.noreply.github.com>
2025-09-30 23:17:04 +0800

    修复脚本调用错误，将 wrangler-to-r2.sh 更改为正确的文件名

--- src/scripts/prepare.js
+++ src/scripts/prepare.js
@@ -2,5 +2,5 @@ const { execSync } = require("child_process");
 
 execSync("node src/scripts/generate-lqip.js", { stdio: "inherit" });
 execSync("node src/scripts/generate-json.js", { stdio: "inherit" });
-execSync("src/scripts/wangler-to-r2.sh", { stdio: "inherit" });
+execSync("src/scripts/wrangler-to-r2.sh", { stdio: "inherit" });
 execSync("node src/scripts/generate-index-mdx.js", { stdio: "inherit" });
